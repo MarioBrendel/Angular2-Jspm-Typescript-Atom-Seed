6a05c5795dbebe551cb7a5908bdaca8ccd9ea395
Cirby87 <Cirby87@gmx.de>
2016-01-28 17:51:02 +0100

    Setup
    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'app',
+  template: `
+  <p>Hello World</p>
+  `
+})
+export class AppComponent {
+  constructor() { }
+}
---
+++ app/bootstrap.ts
@@ -0,0 +1,5 @@
+import { bootstrap } from 'angular2/platform/browser';
+import {AppComponent} from "./app.component";
+import 'relfect-metadata';
+
+bootstrap(AppComponent);
